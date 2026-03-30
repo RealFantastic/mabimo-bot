2a26376377f5cec817d3b7bada92eba635fe1dd0
RealFantastic <wjdghks5698@gmail.com>
2026-03-30 18:07:16 +0900

    fixed untracked files

--- app/collectors/notice.py
+++ app/collectors/notice.py
@@ -1,10 +1,14 @@
 #imports
 from bs4 import BeautifulSoup
+from lxml.doctestcompare import strip
+
 from app.utils.http import get
 
 
 # app/collectors/notice.py
 
+#BASE_URL
+BASE_URL = "https://mabinogimobile.nexon.com"
 # 공지사항 URL 작성
 NOTICE_URL = "https://mabinogimobile.nexon.com/News/notice"
 
@@ -12,18 +16,36 @@ def fetch_notice_list():
     html = get(NOTICE_URL)
     soup = BeautifulSoup(html, "lxml")
 
-    notices = []
-    items = soup.select("a") # 임시용 a 태그만 추출
+    notices: list[dict] = []
+    list_area = soup.select_one('div.list_area[data-mm-boardlist]')
+    if not list_area:
+        print("[WARNING] Notice list area not found.")
+        return notices
 
-    for item in items[:10]:
-        title = item.get_text(strip=True)
-        url = item.get("href")
+    items = list_area.select('li.item[data-mm-listitem][data-threadid]')
 
-        if not title or not url:
+    for item in items:
+        thread_id = item.get("data-threadid", "").strip()
+        if not thread_id:
             continue
-        if not url.startswith("http"):
-            url = "https://mabinogimobile.nexon.com" + url
 
-        notices.append({"title": title, "url": url})
+        type_tag = item.select_one("div.order_1 div.type > span")
+        title_tag = item.select_one("div.order_1 a.title > span")
+        published_at_tag = item.select_one("div.order_2 div.sub_info div.date > span")
+
+        category = type_tag.get_text(strip=True) if type_tag else ""
+        title = title_tag.get_text(strip=True) if title_tag else ""
+        published_at = published_at_tag.get_text(strip=True) if published_at_tag else ""
+
+        if not title:
+            continue
+        url = f"{BASE_URL}/News/Notice/{thread_id}"
+        notices.append({
+            "thread_id": thread_id,
+            "category": category,
+            "title": title,
+            "url": url,
+            "published_at": published_at
+        })
 
     return notices
--- app/main.py
+++ app/main.py
@@ -8,7 +8,10 @@ def main():
 
     print("=== 공지사항 목록 ===")
     for notice in notices:
+        print(f"글번호 : {notice['thread_id']}")
         print(f"제목 : {notice['title']}")
+        print(f"분류 : {notice['category']}")
+        print(f"날짜 : {notice['published_at']}")
         print(f"URL : {notice['url']}")
         print("-" * 50)
 
